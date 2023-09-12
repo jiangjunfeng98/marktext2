f947a7b5395d14476a665cd97b78572beca54085
几个霜 <510879374@qq.com>
2023-09-12 22:33:11 +0800

    init

---
+++ src/renderer/src/App.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+// import Versions from './components/Versions.vue'
+</script>
+
+<template>
+  <div class="text-3xl font-bold underline">Hello world!</div>
+</template>
+
+<style lang="less"></style>
